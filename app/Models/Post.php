b4d24aa1edf414cf9e8c52b24ba91047eb62e86d
fsraaa077 <stfaradilla785@gmail.com>
2026-04-30 00:33:54 +0700

    Add admin frontend for article management, configure project environment, and update Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'slug',
+        'location',
+        'body',
+        'category',
+        'status',
+        'user_id',
+        'views',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(\App\Models\User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(\App\Models\Comment::class);
+    }
 }
